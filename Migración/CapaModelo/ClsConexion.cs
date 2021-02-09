b74f3e88ea62ab204206906eba4078e3dcff4dae
Jose09011712 <jlopezt11@miumg.edu.gt>
2021-02-09 14:36:57 -0600

    Conexion

--- Migración/CapaModelo/ClsConexion.cs
+++ Migración/CapaModelo/ClsConexion.cs
@@ -1,12 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Odbc;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CapaModelo
 {
-    class ClsConexion
+    public class clsConexion
     {
+        public OdbcConnection conexion()
+        {
+            //creacion de la conexion via ODBC
+            OdbcConnection conn = new OdbcConnection("Dsn=Migracion");
+            try
+            {
+                conn.Open();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+            return conn;
+        }
+
+        //metodo para cerrar la conexion
+        public void desconexion(OdbcConnection conn)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (OdbcException)
+            {
+                Console.WriteLine("No Conectó");
+            }
+        }
     }
 }
